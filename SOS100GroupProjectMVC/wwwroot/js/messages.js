790294edcd26418f5d4c8e5d706e6fbe45cedfee
jojo-git67 <joakim.johansson.16@student.hv.se>
2026-03-12 10:25:25 +0100

    Added conditional rendering to messages-preview

---
+++ SOS100GroupProjectMVC/wwwroot/js/messages.js
@@ -0,0 +1,9 @@
+const messages = document.getElementsByClassName("messages-preview");
+
+//TODO: change to foreach loop?
+for(let i = 0; i < messages.length; i++){
+    messages[i].addEventListener("click", function(){
+        //console.log(messages[i].id);
+        //TODO: Add valid functionality
+    })
+}
