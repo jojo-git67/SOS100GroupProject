023d9eecdadec8f465a7ad6a5503a175e82d6451
jojo-git67 <joakim.johansson.16@student.hv.se>
2026-03-12 14:15:22 +0100

    Created userDb

--- SOS100GroupProjectMVC/Program.cs
+++ SOS100GroupProjectMVC/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SOS100GroupProjectMVC.Data;
+
 namespace SOS100GroupProjectMVC;
 
 public class Program
@@ -8,6 +11,10 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
+        
+        builder.Services.AddDbContext<UserDbContext>(options =>
+            options.UseSqlite(
+                builder.Configuration.GetConnectionString("DefaultConnection")));
 
         var app = builder.Build();
 
