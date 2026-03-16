6b20c5878eaaaaa09d874dfdf810dea837ca63e8
jojo-git67 <joakim.johansson.16@student.hv.se>
2026-03-16 15:50:29 +0100

    Updated messages.cshtml to get data from database

---
+++ SOS100GroupProjectMVC/DTOs/Message.cs
@@ -0,0 +1,11 @@
+namespace SOS100GroupProjectMVC.DTOs;
+
+public class Message
+{
+    public int Id { get; set; }
+    public int SenderId { get; set; }
+    public int ReceiverId { get; set; }
+    public string Content { get; set; }
+    public DateTime timestamp { get; set; }
+    public bool IsRead { get; set; }
+}
