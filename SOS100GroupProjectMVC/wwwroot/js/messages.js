4d1bc1e3f219e7b2c35b35e7c5c0a14c8a437eea
jojo-git67 <joakim.johansson.16@student.hv.se>
2026-03-16 16:15:59 +0100

    Added comment in messages.js

--- SOS100GroupProjectMVC/wwwroot/js/messages.js
+++ SOS100GroupProjectMVC/wwwroot/js/messages.js
@@ -3,6 +3,7 @@ const messages = document.getElementsByClassName("messages-preview");
 for(let i = 0; i < messages.length; i++){
     messages[i].addEventListener("click", async function(){
         //console.log(messages[i].id);
+        //TODO: Switch to AJAX
         let inputString = "http://localhost:5282/api/Kommunication/" + messages[i].id + "/read";
         await fetch(inputString, {
             method: "PATCH"
