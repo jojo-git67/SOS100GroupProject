0fa5bc3b126c46f1c95828e04bd90cff54b11871
jojo-git67 <joakim.johansson.16@student.hv.se>
2026-03-18 11:58:02 +0100

    Changed Default route to Login/Index.cshtml

--- SOS100GroupProjectMVC/Program.cs
+++ SOS100GroupProjectMVC/Program.cs
@@ -34,7 +34,7 @@ public class Program
         app.MapStaticAssets();
         app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=Login}/{action=Index}/{id?}")
             .WithStaticAssets();
 
         app.Run();
