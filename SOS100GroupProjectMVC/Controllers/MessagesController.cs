a47270c3af711d729d4f3bb6076cb505803c40c5
jojo-git67 <joakim.johansson.16@student.hv.se>
2026-03-18 17:19:32 +0100

    Added API-call to MessagesController and return View(Messages)

--- SOS100GroupProjectMVC/Controllers/MessagesController.cs
+++ SOS100GroupProjectMVC/Controllers/MessagesController.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using SOS100GroupProjectMVC.DTOs;
 
 namespace SOS100GroupProjectMVC.Controllers;
 
 public class MessagesController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly HttpClient _httpClient;
+
+    public MessagesController(IHttpClientFactory factory)
     {
-        return View();
+        _httpClient = factory.CreateClient();
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        var userId = Request.Cookies["userId"];
+
+        if (string.IsNullOrEmpty(userId))
+        {
+            return RedirectToAction("Index", "Login");
+        }
+
+        var response = await _httpClient.GetAsync(
+            $"http://localhost:5282/api/Kommunication/user/{userId}");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return View(new List<Message>());
+        }
+
+        var messages = await response.Content
+            .ReadFromJsonAsync<List<Message>>();
+
+        return View(messages);
     }
 }
