4809651537f112b1342898cfc917fb58750089ab
jojo-git67 <joakim.johansson.16@student.hv.se>
2026-03-20 14:33:52 +0100

    Removed check if in development mode

--- SOS100GroupProjectMVC/Program.cs
+++ SOS100GroupProjectMVC/Program.cs
@@ -35,7 +35,7 @@ public class Program
         }
 
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+        //if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
             app.UseHsts();
