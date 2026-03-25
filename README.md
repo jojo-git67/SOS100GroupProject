8a2281bd715fdb511f6b1d2fb451ebed2ae71deb
charbell12 <chli0032@student.hv.se>
2026-03-25 13:36:22 +0100

    Remove all untracked build files, keep only source

--- README.md
+++
@@ -1,2 +0,0 @@
-# SOS100GroupProject
-Group Project for SOS100. Group: T1
--- SOS100GroupProjectMVC/DTOs/CreateMessageDto.cs
+++
@@ -1,9 +0,0 @@
-namespace SOS100GroupProjectMVC.DTOs;
-
-public class CreateMessageDto
-{
-    public int SenderId { get; set; }
-    public int ReceiverId { get; set; }
-    public string Content { get; set; }
-    public string Title { get; set; }
-}
