1eaa69f3154a09fecbde5d95df7dc8f41f927392
charbell12 <chli0032@student.hv.se>
2026-03-25 12:05:40 +0100

    Clean repository and apply .gitignore properly

--- CourseCatalogAPI/obj/Debug/net10.0/CourseCatalogAPI.AssemblyInfo.cs
+++ CourseCatalogAPI/obj/Debug/net10.0/CourseCatalogAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseCatalogAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2479a95dcb1f67cf39699d0fc68562ecc1669eba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65df58574a98a020575f0ef0d6f36b7acec9575a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseCatalogAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseCatalogAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Registreringstjansten/Program.cs
+++ Registreringstjansten/Program.cs
@@ -28,8 +28,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Enable Scalar API documentation in development environment
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.MapScalarApiReference();
