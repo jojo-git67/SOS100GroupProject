9ad631d319beb8a7e0935bcbb75659fab8c9dc68
Selma Elouzair <selma.elouzeir@gmail.com>
2026-03-26 15:30:20 +0100

    Enable Swagger in production

--- ScheduleService/Program.cs
+++ ScheduleService/Program.cs
@@ -24,11 +24,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors("AllowAll");
 
