37c902529423514e1fd17b93b06a9dd0363526d9
charbell12 <chli0032@student.hv.se>
2026-03-27 02:56:17 +0100

    added hardcoded users

--- SOS100GroupProjectMVC/Data/UserSeeder.cs
+++ SOS100GroupProjectMVC/Data/UserSeeder.cs
@@ -11,21 +11,25 @@ public static class UserSeeder
     {
         var defaultUsers = new[]
         {
-            // IT-admin
+            // IT-admin (2 st)
             new { UserName = "admin", Salt = "AdminSalt123", Password = "admin!", Email = "admin@example.com", FirstName = "Admin", LastName = "User", Role = "IT-admin" },
             new { UserName = "admin2", Salt = "AdminSalt456", Password = "admin!", Email = "admin2@example.com", FirstName = "Admin2", LastName = "User", Role = "IT-admin" },
 
-            // teacher
+            // teacher (2 st)
             new { UserName = "teacher", Salt = "TeacherSalt123", Password = "teacher!", Email = "teacher@example.com", FirstName = "Teacher", LastName = "User", Role = "teacher" },
             new { UserName = "teacher2", Salt = "TeacherSalt456", Password = "teacher!", Email = "teacher2@example.com", FirstName = "Teacher2", LastName = "User", Role = "teacher" },
 
-            // courseAdmin
+            // courseAdmin (3 st)
             new { UserName = "courseadmin", Salt = "CourseAdminSalt123", Password = "courseadmin!", Email = "courseadmin@example.com", FirstName = "Course", LastName = "Admin", Role = "courseAdmin" },
             new { UserName = "courseadmin2", Salt = "CourseAdminSalt456", Password = "courseadmin!", Email = "courseadmin2@example.com", FirstName = "Course2", LastName = "Admin", Role = "courseAdmin" },
+            new { UserName = "courseadmin3", Salt = "CourseAdminSalt789", Password = "courseadmin!", Email = "courseadmin3@example.com", FirstName = "Course3", LastName = "Admin", Role = "courseAdmin" },
 
-            // student
+            // student (5 st)
             new { UserName = "user", Salt = "UserSalt123", Password = "student!", Email = "user@example.com", FirstName = "Normal", LastName = "User", Role = "student" },
-            new { UserName = "user2", Salt = "UserSalt456", Password = "student!", Email = "user2@example.com", FirstName = "Normal2", LastName = "User", Role = "student" }
+            new { UserName = "user2", Salt = "UserSalt456", Password = "student!", Email = "user2@example.com", FirstName = "Normal2", LastName = "User", Role = "student" },
+            new { UserName = "student3", Salt = "StudentSalt3", Password = "student!", Email = "student3@example.com", FirstName = "Normal3", LastName = "User", Role = "student" },
+            new { UserName = "student4", Salt = "StudentSalt4", Password = "student!", Email = "student4@example.com", FirstName = "Normal4", LastName = "User", Role = "student" },
+            new { UserName = "student5", Salt = "StudentSalt5", Password = "student!", Email = "student5@example.com", FirstName = "Normal5", LastName = "User", Role = "student" }
         };
 
         foreach (var u in defaultUsers)
